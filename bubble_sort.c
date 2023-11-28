edd4d8837f5f8cd0bf68444f5f97632c157daabf
Mert Ali Dugan <duganmrt@gmail.com>
2023-11-28 12:03:41 +0300

    refactored bubble_sort code

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 
-int main(){
-    int n;
-    printf("Kac adet tam sayi gireceksiniz? ");
-    scanf("%d", &n);
+int read_number(char* message) {
+    int number;
+    printf("%s", message);
+    scanf("%d", &number);
+    return number;
+}
 
-    int sayilar[n];
+void fill_array(int* array, int size, char* message) {
+    printf("Lutfen %d adet tam sayi giriniz:\n", size);
+    for (int i = 0; i < size; i++)
+        scanf("%d", &array[i]);
+}
 
-    printf("Lutfen %d adet tam sayi giriniz:\n", n);
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &sayilar[i]);
-    }
-    int temp;
-
-    printf("\nGirilen sayilar:\n");
-    for (int i = 0; i < n; ++i) {
-        printf("-%d\n", sayilar[i]);
-    }
-    for (int i = 0; i < n-1; ++i) {
-        for(int j =0; j < n-i-1; j++){
-            if(sayilar[j] > sayilar[j+1]){
-                temp = sayilar[j];
-                sayilar[j] = sayilar[j+1];
-                sayilar[j+1] = temp;
-            }
-        }
-    }  
-
-    printf("\nSıralanmış hali:\n");
+void print_array(char* title, int* array, int n, char* delimiter) {
+    printf("\n%s\n", title);
     for(int i = 0; i < n; i++){
-        printf("%d", sayilar[i]);
-         if (i < n - 1) {
-            printf("<");
-        }
+        printf("%d", array[i]);
+        if (i < n - 1)
+            printf("%s", delimiter);
     }
     printf("\n");
+}
 
+void bubble_sort(int* array, int n) {
+    for (int i = 0; i < n-1; i++)
+        for (int j = 0; j < n-i-1; j++)
+            if (array[j] > array[j+1])
+                swap(&array[j], &array[j+1]);
+}
+
+int main(){
+    int size = read_number("Kaç adet tam sayı gireceksiniz? ");
+    int array[size];
+    fill_array(array, size, "Lütfen %d adet tam sayı giriniz:\n");
+    print_array("Girilen sayilar:", array, size, ", ");
+    bubble_sort(array, size);
+    print_array("Sıralanmış hali:", array, size, " < ");
     return 0;
 }
